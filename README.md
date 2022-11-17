e45f8f71da562e9c83480f9a37bab33f63802660
Samuel Hornsey <hornseysam498@gmail.com>
2022-11-17 21:46:41 +1100

    updated readme

--- README.md
+++ README.md
@@ -24,4 +24,44 @@ This is a new project that is in very early development stages. This project aim
 
 ## Getting Started
 
-*here we go...*
+```python
+from streaming.app import App
+import streaming.operations as operations
+
+# create a streaming app
+app = App(name='Word Count')
+
+
+class Tokenizer(operations.FlatMapFunction):
+    def flat_map(self, event):
+        return event.lower().split()
+
+
+class KeyIndex(operations.MapFunction):
+    def map(self, event):
+        return self.keyed_event(event, 1)
+
+
+class CountReducer(operations.ReducerFunction):
+    def reduce(self, key, reduced, event):
+        if reduced is None:
+            return 1
+
+        return reduced + event
+
+
+@app.stream()
+def stream_processor(datastream):
+    events = ['event', 'event text', 'event test test',
+              'test text event hello hello']
+    print(f'input events = {events}')
+
+    datastream.source_from_collection(events)
+
+    datastream.flat_map(Tokenizer()) \
+        .map(KeyIndex()) \
+        .reduce(CountReducer())
+
+    datastream.print()
+
+```
--- examples/word_count.py
+++ examples/word_count.py
@@ -1,28 +1,32 @@
 from streaming.app import App
-from streaming.operations import MapFunction, FlatMapFunction, ReducerFunction
+import streaming.operations as operations
 
 # create a streaming app
 app = App(name='Word Count')
 
 
-class Tokenizer(FlatMapFunction):
+class Tokenizer(operations.FlatMapFunction):
     def flat_map(self, event):
         return event.lower().split()
 
-class KeyIndex(MapFunction):
+
+class KeyIndex(operations.MapFunction):
     def map(self, event):
         return self.keyed_event(event, 1)
 
-class CountReducer(ReducerFunction):
+
+class CountReducer(operations.ReducerFunction):
     def reduce(self, key, reduced, event):
-        if reduced == None:
+        if reduced is None:
             return 1
 
         return reduced + event
 
+
 @app.stream()
 def stream_processor(datastream):
-    events = ['event', 'event text', 'event test test', 'test text event hello hello']
+    events = ['event', 'event text', 'event test test',
+              'test text event hello hello']
     print(f'input events = {events}')
 
     datastream.source_from_collection(events)
