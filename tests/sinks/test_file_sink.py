7ee99c54c4361c987fe2ea244f3564226f51779e
Samuel Hornsey <hornseysam498@gmail.com>
2022-11-21 19:06:26 +1100

    added file sink/source tests

---
+++ tests/sinks/test_file_sink.py
@@ -0,0 +1,19 @@
+from streaming.sinks import FileSink
+
+from streaming.app.events import EventCollection
+
+def test_file_sink(monkeypatch):
+    agg = []
+
+    def mock_pipe_capture(event_collection):
+        for event in event_collection.events:
+            agg.append(event)
+
+    sink = FileSink('output.txt')
+    event_collection = EventCollection({'test': 'event'})
+    monkeypatch.setattr(sink, 'pipe', mock_pipe_capture)
+
+    sink.pipe(event_collection)
+
+    assert len(agg) == len(event_collection.events)
+    assert agg[0] == event_collection.events[0]
---
+++ tests/sources/test_file_source.py
@@ -0,0 +1,25 @@
+from streaming.sources import FileSource
+
+
+def test_file_source():
+    file_path = 'examples/data/lorem_ipsum.txt'
+    source = FileSource(file_path)
+    assert source.file == file_path
+
+
+def test_collection_source_generate():
+    file_path = 'examples/data/lorem_ipsum.txt'
+    source = FileSource(file_path)
+
+    f = open(file_path)
+
+    lines = f.readlines()
+    events = []
+
+    for event in source.generate():
+        events.append(event)
+
+    assert len(lines) == len(events)
+
+    for i in range(len(lines)):
+        assert lines[i].strip() == events[i]
