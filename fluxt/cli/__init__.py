044083f78c873fdd99ea2176e588fe62f252b1ff
Samuel Hornsey <hornseysam498@gmail.com>
2022-11-25 23:00:51 +1100

    added basic cli

---
+++ fluxt/cli/__init__.py
@@ -0,0 +1,1 @@
+from fluxt.cli.cli import cli
---
+++ fluxt/cli/cli.py
@@ -0,0 +1,43 @@
+import typer
+import importlib
+import rich
+
+from fluxt import __version__
+
+cli = typer.Typer()
+
+
+@cli.command()
+def run(module_name, app='fluxt'):
+    """ run fluxt app """
+    module = importlib.import_module(module_name)
+    fluxt = getattr(module, app)
+
+    fluxt.run()
+
+
+@cli.command()
+def server():
+    """ start fluxt server """
+    pass
+
+
+@cli.command()
+def submit():
+    """ submit job to fluxt server """
+    pass
+
+
+@cli.command()
+def about():
+    """ show info about fluxt """
+    rich.print('\n'
+          f'███████╗██╗     ██╗   ██╗██╗  ██╗████████╗\n'
+          f'██╔════╝██║     ██║   ██║╚██╗██╔╝╚══██╔══╝\n'
+          f'█████╗  ██║     ██║   ██║ ╚███╔╝    ██║   \n'
+          f'██╔══╝  ██║     ██║   ██║ ██╔██╗    ██║   \n'
+          f'██║     ███████╗╚██████╔╝██╔╝ ██╗   ██║   \n'
+          f'╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   \n\n'
+          f'version: {__version__}\n'
+          f'license: MIT\n'
+          f'source: https://github.com/SamuelHornsey/fluxt\n')
